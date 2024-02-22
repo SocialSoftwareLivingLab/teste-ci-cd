1d2d3f7a7c16f1f3b0cb9ed5e7a2becc20174361
Antonio Junior <antoniojuniorlk366@gmail.com>
2024-02-21 22:01:06 -0300

    adiciona testes ao projeto

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Click on the Vite and React logos to learn more/i);
+  expect(linkElement).toBeInTheDocument();
+});
